93f6c9536f786d87998dfb102b5dbf6abc48b94b
Tram Anh Nguyen <tramanh06@gmail.com>
2018-06-21 10:56:03 +0800

    Reformat code with pep8 + refactor file opening methods

--- pdf2bow/ispdf.py
+++ pdf2bow/ispdf.py
@@ -1,25 +1,27 @@
 import re
 import subprocess as sp
 
+
 def ispdf(fileName):
-  """ Check if file is a pdf or not 
-      Requires the file command
-  """ 
+    """ Check if file is a pdf or not 
+        Requires the file command
+    """
 
-  cmd = "file" # -kb ", fileName)
-  flags = "-kb"
-  arg  = fileName
+    cmd = "file"  # -kb ", fileName)
+    flags = "-kb"
+    arg = fileName
 
-  # Call `file fileName`
-  proc = sp.Popen([cmd, flags, arg], stdout=sp.PIPE)
-  proc.wait()
-  result = proc.stdout.readline()
+    # Call `file fileName`
+    proc = sp.Popen([cmd, flags, arg], stdout=sp.PIPE)
+    proc.wait()
+    result = proc.stdout.readline()
 
-  if re.match('[ ]*PDF', result):
-    return True
+    if re.match('[ ]*PDF', result):
+        return True
 
-  return False
+    return False
 
 
 if __name__ == "__main__":
-    ispdf()
+    is_pdf = ispdf("/Users/nus/git/tpms/paper.pdf")
+    assert(is_pdf)
